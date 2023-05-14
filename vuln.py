909364b66ea1057fbea18aa72a8bb819e069b103
chethan308 <chethan308@yahoo.com>
2023-05-14 16:11:12 -0700

    Update vuln.py

--- vuln.py
+++ vuln.py
@@ -7,5 +7,10 @@ def hello():
     template = f"<p>Hello {username}</p>" # User input is used directly in the string to be rendered
     return render_template_string(template) # Noncompliant
   
+def example():
+    operation = request.args.get("operation")
+    eval(f"product_{operation}()") # Noncompliant
+    return "OK"
+
 my_list = [3, 4, 5, 6]
 my_list[15]
